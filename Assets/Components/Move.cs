3170fa27fe2fa7d61b35d137f6b10b40fba56693
T-800 <adequin.renaud@gmail.com>
2016-10-19 10:46:03 +0200

    first commit

---
+++ Assets/Components/Move.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+
+	public float vitesse = 2.5f;
+}
