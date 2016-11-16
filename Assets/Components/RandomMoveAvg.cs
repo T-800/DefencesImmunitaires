977fb5fbc9ca44e4ff3ee0bf3afffb4147e91ad7
T-800 <adequin.renaud@gmail.com>
2016-11-16 14:33:02 +0100

    randommove

---
+++ Assets/Components/RandomMoveAvg.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMoveAvg : MonoBehaviour {
+
+	public float reloadTime;
+	public bool left;
+	public float reloadProgress;
+	// Use this for initialization
+	void Start () {
+		reloadTime = Random.Range (2f, 5f);
+		left = Random.Range (0, 2) == 0;
+		reloadProgress = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void randomrange(){
+		reloadTime = Random.Range (1f, 2f);
+		left = Random.Range (0, 2) == 0;
+		reloadProgress = 0f;
+	}
+}
---
+++ Assets/Systems/RandomMoveSystemVert.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class RandomMoveSystemVert : FSystem {
+
+	private Family _controllableGO = FamilyManager.getFamily(new AllOfComponents(typeof(Move)),new AnyOfComponents(typeof(RandomMoveAvg), typeof(RandomMoveFast)));
+
+
+
+	// Use this to update member variables when system pause. 
+	// Advice: avoid to update your families inside this function.
+	protected override void onPause(int currentFrame) {
+	}
+
+	// Use this to update member variables when system resume.
+	// Advice: avoid to update your families inside this function.
+	protected override void onResume(int currentFrame){
+	}
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+		foreach (GameObject go in _controllableGO) {
+
+			RandomMoveAvg randomMoveAvg = go.GetComponent<RandomMoveAvg> ();
+			RandomMoveFast randomMoveFast = go.GetComponent<RandomMoveFast> ();
+
+
+
+			float time;
+			float reloadProgress;
+			bool left;
+
+
+			if (randomMoveAvg != null) {
+				time = randomMoveAvg.reloadTime;
+				randomMoveAvg.reloadProgress += Time.deltaTime;
+				reloadProgress = randomMoveAvg.reloadProgress;
+				left = randomMoveAvg.left;
+			} else {
+				time = randomMoveFast.reloadTime;
+				randomMoveFast.reloadProgress += Time.deltaTime;
+				reloadProgress = randomMoveFast.reloadProgress;
+				left = randomMoveFast.left;
+			}
+
+
+			if (reloadProgress >= time){
+				if (randomMoveAvg != null) {
+					randomMoveAvg.randomrange ();
+				} else if (randomMoveFast != null) {
+					randomMoveFast.randomrange ();
+				}
+
+			}
+			Transform tr = go.GetComponent<Transform> ();
+			Move mv = go.GetComponent<Move> ();
+
+			Vector3 movement = Vector3.zero;
+
+			if (left) {
+				movement += Vector3.left;
+			}
+			else {
+				movement += Vector3.right;
+			}
+			tr.position += movement * mv.vitesse * Time.deltaTime;
+
+		}
+	}
+}
